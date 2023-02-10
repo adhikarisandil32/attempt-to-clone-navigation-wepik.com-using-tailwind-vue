a62696e5f40d921dde7fdb8b5900f3b859f40a81
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-02-10 23:07:18 +0545

    commit

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -66,21 +66,159 @@
         </div>
 
         <main>
-            <div class="py-8 bg-gray-900">
-                <div class="w-fit mx-auto">
-                    <img src="../assets/logo-wepik-for-darkbg.svg" alt="wepik_logo" class="h-8">
-                    <p class="cursor-pointer mt-4 text-gray-500 hover:text-gray-400 text-center">
-                        support@wepik.com
-                    </p>
+            <div class="py-8 bg-gray-900 border-b border-b-gray-600 ">
+                <div class="sm:p-4 sm:flex sm:align-top sm:justify-between">
+                    <div class="mt-8">
+                        <div class="w-fit mx-auto">
+                            <img src="../assets/logo-wepik-for-darkbg.svg" alt="wepik_logo" class="h-8">
+                            <p class="cursor-pointer mt-3 text-gray-500 hover:text-gray-400 text-center sm:text-left">
+                                support@wepik.com
+                            </p>
+                        </div>
+        
+                        <div class="w-fit mx-auto sm:mx-0 mt-8">
+                            <p class="text-gray-300 font-bold">
+                                SOCIAL MEDIA
+                            </p>
+                            <div class="mt-4 py-1 px-2 w-fit text-white bg-red-600 rounded-md text-xl">
+                                <a href="#">
+                                    <font-awesome-icon icon="fa-brands fa-pinterest" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="mt-8 px-8">
+                        <p class="font-bold text-gray-400">
+                            SMALL BUSINESS GRAPHICS
+                        </p>
+                        <div class="mt-4 text-sm text-gray-500 flex justify-between">
+                            <ul>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Restaurant</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Coffee Shop</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Real Estate</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Photographer</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Beauty Salon</a>
+                                </li>
+                            </ul>
+                            <ul class="mr-8 sm:mr-4">
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Personal Trainer</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Spa</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Bakery</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block w-fit py-1 hover:text-gray-400">Travel Agency</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+    
+                    <div class="mt-8 px-6 flex justify-between">
+                        <div class="w-fit mr-20">
+                            <p class="font-bold text-gray-400">
+                                FEATURES
+                            </p>
+                            <div class="mt-4 text-sm text-gray-500 flex justify-between">
+                                <ul>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Photo Editor</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">AI Generator</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Brand Kit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Background Remover</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Mobile Apps</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Social Media Scheduler</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+    
+                        <div class="w-fit">
+                            <p class="font-bold text-gray-400">
+                                HELP
+                            </p>
+                            <div class="mt-4 text-sm text-gray-500 flex justify-between">
+                                <ul class="w-1/2">
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Support</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Terms & conditions</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Privacy policy</a>
+                                    </li>
+                                </ul>
+                                <ul class="w-1/2">
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">About Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block w-fit py-1 hover:text-gray-400">Cookie policy</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    
+
+                <div class="mt-12 sm:px-8 text-gray-500 flex gap-x-4 justify-center sm:justify-end">
+                    <a href="#">
+                        <div class="px-4 py-1 w-fit border border-gray-300 rounded-lg flex hover:text-gray-300">
+                            <p class="text-4xl mr-4">
+                                <font-awesome-icon icon="fa-brands fa-apple" class=""/>
+                            </p>
+                            <p class="leading-4">
+                                <span class="text-xs">Download on the</span><br>
+                                <span class="font-bold text-base">App Store</span>
+                            </p>
+                        </div>
+                    </a>
+
+                    <a href="#">
+                        <div class="px-4 py-1 w-fit border border-gray-300 rounded-lg flex hover:text-gray-300">
+                            <p class="text-4xl mr-4">
+                                <font-awesome-icon icon="fa-brands fa-google-play"/>
+                            </p>
+                            <p class="leading-4">
+                                <span class="text-xs">Get it on</span><br>
+                                <span class="font-bold text-base">Google Play</span>
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
 
-            <div></div>
+            <!-- <div class="bg-gray-900">
+                <div>
+                    <img src="../assets/freepikcompany.svg" alt="freepikcompany">
+                </div>
+            </div>
 
-            <div></div>
+            <div></div> -->
         </main>
 
     </footer>
--- src/main.js
+++ src/main.js
@@ -12,9 +12,12 @@ import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faPinterest } from '@fortawesome/free-brands-svg-icons'
+import { faApple } from '@fortawesome/free-brands-svg-icons'
+import { faGooglePlay } from '@fortawesome/free-brands-svg-icons'
 
 
 /* add icons to the library */
-library.add(faMagnifyingGlass,faChevronDown,faUser,faGlobe,faBars,faQuoteLeft)
+library.add(faMagnifyingGlass,faChevronDown,faUser,faGlobe,faBars,faQuoteLeft,faPinterest,faApple,faGooglePlay)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
